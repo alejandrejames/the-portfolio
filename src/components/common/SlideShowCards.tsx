e1ea5996cbf17d9321b3f97c2c71a9ee97c05fa0
Alejandre James Papina <alejandrejamespapina@Cygnus.local>
2025-09-19 17:32:27 +0800

    Add tech mainvisual slider

---
+++ src/components/common/SlideShowCards.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import { FaLinux, FaPhp, FaDocker, FaWordpress, FaReact, FaGulp, FaSymfony } from "react-icons/fa"
+import { SiApache, SiMysql, SiTypescript, SiOpenai,SiJavascript, SiMongodb, SiExpress, SiNodedotjs, SiTailwindcss, SiAstro } from "react-icons/si";
+import { Card, CardContent } from "@/components/ui/card";
+import { useState, useEffect, useRef } from 'react';
+
+const tiles = [
+  FaLinux,
+  SiApache,
+  SiMysql,
+  FaPhp,
+  FaDocker,
+  SiTypescript,
+  FaWordpress,
+  SiOpenai,
+  SiJavascript,
+  SiMongodb,
+  SiExpress,
+  FaReact,
+  SiNodedotjs,
+  FaGulp,
+  FaSymfony,
+  SiTailwindcss,
+  SiAstro
+];
+
+function shuffleArray<T>(array: T[]): T[] {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+}
+
+export default function SlideShowCards() {
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(()=> {
+    const windowsResized = () => {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    window.addEventListener('resize', windowsResized);
+
+    windowsResized();
+
+    return () => {
+      window.removeEventListener('resize', windowsResized);
+    };
+  }, []);
+
+  const cardRef = useRef<HTMLDivElement | null>(null);
+  const cardSize = cardRef.current?.getBoundingClientRect();
+
+  let numRows = 20 as number;
+  let numColumns = 20 as number;
+  
+  if (cardSize) {
+    numRows = Math.floor(windowDimensions.width / cardSize?.width) * Math.floor(windowDimensions.height / cardSize?.height);
+    numColumns = Math.floor(numRows / tiles.length) * Math.floor(windowDimensions.height / cardSize?.height);
+  }
+
+  return (
+    <>
+      <div className="
+        absolute
+        bottom-0
+        left-0
+        inset-0
+        z-1
+        bg-gradient-to-b from-white/60 via-white/40 to-white/20
+      dark:from-black/30 dark:via-black/20 dark:to-black/10
+        max-md: opacity-70
+      ">
+        <div className="
+          absolute
+          right-0
+          bottom-0
+          inset-0
+          z-0
+          flex
+          justify-end
+          items-center
+        ">
+          <div className="rotate-6 scale-110">
+            <div className="flex flex-col animate-vertical-loop gap-5">
+              { Array.from({ length: numColumns }).map((_, colIndex) => (
+                <div key={colIndex} className="flex gap-2">
+                  {shuffleArray(tiles).map((IconComponent, idx) => (
+                    <Card
+                      key={idx}
+                      ref={cardRef}
+                      className="overflow-hidden rounded-2xl shadow-lg p-5 flex items-center justify-center"
+                    >
+                      <CardContent className="p-0">
+                        <IconComponent className="text-black dark:text-gray-300" />
+                      </CardContent>
+                    </Card>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
